3e3e3c9946969065fe1c4955836fee3a22623570
Domenico Luciani <domenico.luciani@email.it>
2013-04-19 16:17:14 +0300

    Update script.js
    Added gist and comments

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ document.addEventListener ("DOMContentLoaded", function() {
     pmessagenew.setAttribute("id", "pmessage");
     pmessagenew.setAttribute("class", "error");
     
+    //Video Tag
     var youtube = document.createElement("input");
     youtube.setAttribute("type", "button");
     youtube.setAttribute("style", "float:left; margin-top:5px");
@@ -20,38 +21,41 @@ document.addEventListener ("DOMContentLoaded", function() {
     	var url = prompt("Inserisci url video:","");
     	if (url!=null && url!=""){
     		var title = prompt("Inserisci titolo del video","");
-    		if(title!=null && title!=""){
+    		if(title && title!=""){
         		document.getElementById ("frmtxt").value += '[yt]'+url+'[\/yt][b]'+title+'[\/b]';
         		} else { document.getElementById ("frmtxt").value += '[yt]'+url+'[\/yt]'; }
         	}else { window.alert("Non hai inserito nessun URL"); }
 
     }, false);
-
+    
+    //Image Tag
     var img = document.createElement("input");
     img.setAttribute("type", "button");
     img.setAttribute("style", "float:left; margin-top:5px");
     img.setAttribute("value", "Image");
     img.addEventListener ("click", function() {
     	var url = prompt("Inserisci url immagine:","");
-    	if (url!=null && url!=""){ 
+    	if (url && url!=""){ 
         	document.getElementById ("frmtxt").value += '[img]'+url+'[\/img]';
         	}else { window.alert("Non hai inserito nessun URL"); }
     }, false);
 
+    //Link Tag
     var url = document.createElement("input");
     url.setAttribute("type", "button");
     url.setAttribute("style", "float:left; margin-top:5px");
     url.setAttribute("value", "Link");
     url.addEventListener ("click", function() {
     	var url = prompt("Inserisci url:","");
-    	if (url!=null && url!=""){
+    	if (url && url!=""){
     		var title = prompt("Inserisci titolo per l'URL","");
-    	if(title!=null && title!=""){
+    	if(title && title!=""){
         	document.getElementById ("frmtxt").value += '[url='+url+']'+title+'[\/url]';
         } else { document.getElementById ("frmtxt").value += '[url]'+url+'[\/url]'; }
         } else { window.alert("Non hai inserito nessun URL"); }
     }, false);
 
+    //Wiki Tag
     var wiki = document.createElement("input");
     wiki.setAttribute("type", "button");
     wiki.setAttribute("style", "float:left; margin-top:5px");
@@ -60,6 +64,7 @@ document.addEventListener ("DOMContentLoaded", function() {
         document.getElementById ("frmtxt").value += '[wiki=it][\/wiki]';
     }, false);
 
+    //Spoiler Tag
     var spoiler = document.createElement("input");
     spoiler.setAttribute("type", "button");
     spoiler.setAttribute("style", "float:left; margin-top:5px");
@@ -68,6 +73,7 @@ document.addEventListener ("DOMContentLoaded", function() {
         document.getElementById ("frmtxt").value += '[spoiler][\/spoiler]';
     }, false);
 
+    //Code Tag
     var code = document.createElement("input");
     code.setAttribute("type", "button");
     code.setAttribute("style", "float:left; margin-top:5px");
@@ -79,8 +85,19 @@ document.addEventListener ("DOMContentLoaded", function() {
         } else { window.alert("Non hai inserito un linguaggio!"); }
     }, false);
     
-
+    //Gist Tag
+    var gist = document.createElement("input");
+    gist.setAttribute("type", "button");
+    gist.setAttribute("style", "float:left; margin-top:5px");
+    gist.setAttribute("value", "Gist");
+    gist.addEventListener ("click", function() {
+      var id_gist = prompt("Inserisci l'id del gist:","");
+    	if(id_gist && id_gist != ""){
+        document.getElementById ("frmtxt").value += '[gist]'+id_gist+'[\/gist]';
+        } else { window.alert("Non hai inserito l'id!"); }
+    }, false);
     
+    //Cur Tag
     var cor = document.createElement("input");
    	cor.setAttribute("type", "button");
     	cor.setAttribute("style", "float:right; margin-right:120px;");
@@ -89,6 +106,7 @@ document.addEventListener ("DOMContentLoaded", function() {
         document.getElementById ("frmtxt").value += '[cur][\/cur]';
     }, false);
     	
+    //Small Tag  
     var small = document.createElement("input");
    	small.setAttribute("type", "button");
     	small.setAttribute("style", "float:right;");
@@ -97,6 +115,7 @@ document.addEventListener ("DOMContentLoaded", function() {
         document.getElementById ("frmtxt").value += '[small][\/small]';
     }, false);
 
+    //Del tag
     var del = document.createElement("input");
    	del.setAttribute("type", "button");
     	del.setAttribute("style", "float:right; ");
@@ -104,7 +123,8 @@ document.addEventListener ("DOMContentLoaded", function() {
     	del.addEventListener ("click", function() {
         document.getElementById ("frmtxt").value += '[del][\/del]';
     }, false);
-    	
+    
+    //U Tag
     var u = document.createElement("input");
    	u.setAttribute("type", "button");
     	u.setAttribute("style", "float:right; ");
@@ -113,6 +133,7 @@ document.addEventListener ("DOMContentLoaded", function() {
         document.getElementById ("frmtxt").value += '[u][\/u]';
     }, false);
 
+    //Bold Tag
     var b = document.createElement("input");
    	b.setAttribute("type", "button");
     	b.setAttribute("style", "float:right; ");
@@ -121,8 +142,7 @@ document.addEventListener ("DOMContentLoaded", function() {
         document.getElementById ("frmtxt").value += '[b][\/b]';
     }, false);
     	
-
-    
+    //Formatting Tag
     var testo = document.createElement("input");
     testo.setAttribute("type", "button");
     testo.setAttribute("style", "float:left; margin-top:5px");
@@ -136,7 +156,6 @@ document.addEventListener ("DOMContentLoaded", function() {
     }, false);
     
 
-
     nerdz_form.removeChild(nerdz_form.childNodes[3]);
     nerdz_form.appendChild(youtube);
     nerdz_form.appendChild(img);
@@ -144,9 +163,8 @@ document.addEventListener ("DOMContentLoaded", function() {
     nerdz_form.appendChild(wiki);
     nerdz_form.appendChild(spoiler);
     nerdz_form.appendChild(code);
+    nerdz_form.appendChild(gist);
     nerdz_form.appendChild(testo);
     nerdz_form.appendChild(pmessagenew);
-    
-
     
 }, false);
