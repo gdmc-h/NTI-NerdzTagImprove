6bdbefac38c2869c6ab5cf35bd2ffae9d6c3998b
JustHvost <we.are.lol.123456789@gmail.com>
2013-04-24 22:04:17 +0300

    Updated Spoiler tag

--- script.user.js
+++ script.user.js
@@ -3,7 +3,7 @@
 // @namespace   tag: nerdz, tag, improve
 // @description Easy-to-use tags button under post forms.
 // @match       http://www.nerdz.eu/*
-// @version     1.1
+// @version     1.2
 // @grant       none
 // ==/UserScript==
 
@@ -16,7 +16,7 @@
 
 
 
-(function() {
+(function() { 
     var generalUtils = {
         inject: function (cb) {
             var elm = document.createElement ("script");
@@ -41,6 +41,7 @@
                 TEXTBOX          = document.getElementById ("frmtxt"),
                 PROTHEME_SUPPORT = localStorage.NTIPThemeSupport === "true",
                 IS_WEBKIT        = /webkit/.test (navigator.userAgent.toLowerCase()),
+                _AVAILABLE_BTNS  = {},
                 _INTERNAL_F_BUTTON = false,
                 // allows to move the cursor in a textbox where you want
                 // thanks to http://stackoverflow.com/a/512542
@@ -92,40 +93,18 @@
                     }
                 },
                 addToTextBoxWithPos = function (text, caretPos) {
-			//Yep... JustHvost was here...
-                    if (document.selection)
-                    {
-                        TEXTBOX.focus();
-                        var sel = document.selection.createRange();
-                        sel.text = text;
-                        TEXTBOX.focus();
-                    }
-                    else if (TEXTBOX.selectionStart || TEXTBOX.selectionStart === 0)
-                    {
-                        var startPos  = TEXTBOX.selectionStart;
-                        var endPos    = TEXTBOX.selectionEnd;
-                        var scrollTop = TEXTBOX.scrollTop;
-                        TEXTBOX.value = TEXTBOX.value.substring (0, startPos) + text + TEXTBOX.value.substring (endPos, TEXTBOX.value.length);
-                        TEXTBOX.focus();
-                        TEXTBOX.selectionStart = startPos + text.length;
-                        TEXTBOX.selectionEnd   = startPos + text.length;
-                        TEXTBOX.scrollTop = scrollTop;
-                    }
-                    else
-                    {
-                        TEXTBOX.value += text;
-                        TEXTBOX.focus();
-                    }
-                         if (caretPos != null && caretPos > -1)
-                         setCaretPosition (TEXTBOX, TEXTBOX.selectionStart - caretPos);
+                    // JustHvost was here! (and maybe Robertof)
+                    addToTextBox (text);
+                    if (caretPos != null && caretPos > -1)
+                        setCaretPosition (TEXTBOX, TEXTBOX.selectionStart - caretPos);
                 },
-                createButton = function (value, listener, customStylingCode, _hidden, _noHideClass) {
+                createButton = function (value, listener, customStylingCode, _hidden, _noHideClass, notShown) {
                     // we may want to use jquery here since we have it, but
                     // we're lazy and we'll use normal DOM functions
                     if (!NERDZ_FORM) return;
                     var btn = document.createElement ("input");
-                    btn.setAttribute ("type", "button");
-                    btn.setAttribute ("value", value);
+                    btn.type = "button";
+                    btn.value = value;
                     btn.style.cssFloat  = "left";
                     btn.style.marginTop = "5px";
                     if (PROTHEME_SUPPORT)
@@ -145,7 +124,8 @@
                     if (typeof customStylingCode === "function")
                         customStylingCode.call (btn);
                     btn.addEventListener ("click", listener, false);
-                    NERDZ_FORM.insertBefore (btn, PMESSAGE);
+                    _AVAILABLE_BTNS[value] = [ btn, !notShown ];
+                    console.log ("[NTI] Registered button {0}, hidden: {1}, no-hide-class: {2}".format (value, _hidden, _noHideClass));
                 },
                 createPromptButton = function (value, question, tagFormat, caretPos, customStyling, _hidden) {
                     createButton (value, function() {
@@ -158,19 +138,104 @@
                         }
                     }, customStyling, _hidden);
                 },
-                createAppendButton = function (value, whatToAppend, caretPos, customStyling, _hidden) {
+                createAppendButton = function (value, whatToAppend, caretPos, customStyling, _hidden, notShown) {
                     createButton (value, function() {
                         addToTextBox (whatToAppend);
                         // NOTE: here I use selectionStart, where in other functions I
                         // used legacy support for old IEs. I realized this isn't needed
                         // since:
-                        // - IE doesn't support userscripts
+                        // - IE doesn't support userscripts (YAYYYYY -JustHvost)
                         // - IE >= 8 handles selectionStart better than previous versions
                         // but I'm too lazy to drop support for IE in other functions since
                         // they are not mine. Feel free to to it!
                         if (caretPos != null && caretPos > -1)
                             setCaretPosition (TEXTBOX, TEXTBOX.selectionStart - caretPos);
-                    }, customStyling, _hidden);
+                    }, customStyling, _hidden, false, notShown);
+                },
+                OptionsAPI = {
+                    registeredOptions: {},
+                    _currentInterval:  null,
+                    registerOption: function (uniqId, name, subName, generateSecondTdCb)
+                    {
+                        if (typeof generateSecondTdCb !== "function" || uniqId in OptionsAPI.registeredOptions)
+                            return;
+                        OptionsAPI.registeredOptions[uniqId] = {
+                            uniqId       : uniqId,
+                            firstTd      : name,
+                            firstTd_sName: subName,
+                            secondTdCb   : generateSecondTdCb,
+                            api          : OptionsAPI,
+                            get          : function (key) {
+                                return OptionsAPI.get (uniqId, key);
+                            },
+                            getBool      : function (key) {
+                                return OptionsAPI.getBool (uniqId, key);
+                            },
+                            store        : function (key, value) {
+                                OptionsAPI.store (uniqId, key, value);
+                            }
+                        };
+                    },
+                    registerCheckboxOption: function (uniqId, name, subName)
+                    {
+                        OptionsAPI.registerOption (uniqId, name, subName, function() {
+                            var checkbox = document.createElement ("input"), context = this;
+                            checkbox.type = "checkbox";
+                            checkbox.checked = context.getBool (context.uniqId);
+                            checkbox.addEventListener ("click", function() {
+                                context.store (context.uniqId, this.checked);
+                            }, false);
+                            return checkbox;
+                        });
+                    },
+                    store: function (uniqId, key, value)
+                    {
+                        var realValue = ( typeof value === "boolean" ? ( value ? "true" : "false" ) : value );
+                        localStorage["OptionsAPI_" + uniqId] = realValue;
+                    },
+                    get: function (uniqId, key)
+                    {
+                        return localStorage["OptionsAPI_" + uniqId];
+                    },
+                    getBool: function (uniqId, key)
+                    {
+                        var sRet = OptionsAPI.get (uniqId, key);
+                        return (sRet !== undefined && sRet === "true");
+                    },
+                    hookIfNecessary: function()
+                    {
+                        // needs to be called only once, hooks into preferences.php only
+                        if (/preferences\.php$/.test (document.location.href))
+                        {
+                            OptionsAPI._currentInterval = setInterval (OptionsAPI._hookOnce, 1000);
+                            setInterval (function() {
+                                if (OptionsAPI._currentInterval == null && !document.getElementById ("edprofrm"))
+                                    OptionsAPI._currentInterval = setInterval (OptionsAPI._hookOnce, 1000);
+                            }, 1500);
+                        }
+                    },
+                    _hookOnce: function() {
+                        if (!document.getElementById ("edprofrm")) return;
+                        clearInterval (OptionsAPI._currentInterval);
+                        OptionsAPI._currentInterval = null;
+                        var trList  = document.getElementsByTagName ("tr");
+                        var insertb = document.getElementsByTagName ("tbody")[1];
+                        for (key in OptionsAPI.registeredOptions)
+                        {
+                            var mainTr = document.createElement ("tr");
+                            var option = OptionsAPI.registeredOptions[key];
+                            var optionHeader = document.createElement ("td");
+                            optionHeader.innerHTML = "<br>" + option.firstTd;
+                            if (option.firstTd_sName)
+                                optionHeader.innerHTML += "<br><p class='small'>{0}</p>".format (option.firstTd_sName);
+                            var sTd = document.createElement ("td");
+                            sTd.appendChild (option.secondTdCb.call (option));
+                            mainTr.appendChild (optionHeader);
+                            mainTr.appendChild (sTd);
+                            insertb.insertBefore (mainTr, trList[trList.length - 1]);
+                        }
+                        document.getElementsByTagName ("tbody")[1].insertBefore (mainTr, trList[trList.length - 1]);
+                    }
                 };
             /*
              * Note for those who will edit the code
@@ -198,6 +263,7 @@
              *   {1}, ecc.
              * Enjoy
              */
+            // TODO: fix bad links with #! at the end
             createAppendButton ("Video", "[yt][/yt]", 5);
             // TODO: localization? Will be possible with APIs maybe
             createAppendButton ("Immagine", "[img][/img]", 6);
@@ -215,15 +281,23 @@
                 }
             });
             // note: 7 is the length of the [/wiki] tag
-            createAppendButton ("Spoiler", "[spoiler][/spoiler]", 10);
+            
+            //Spoiler tag update. -JustHvost
+            createButton ("Spoiler", function() {
+                var spoiler = prompt ("Argomento:", "");
+                if (spoiler != null && spoiler != "")
+                    addToTextBoxWithPos ("[spoiler={0}][/spoiler]".format (nome), 8);
+                else
+                    addToTextBoxWithPos ("[spoiler][/spoiler]", 8);
+            }, null, true);
+            
             createPromptButton ("Codice", "Inserisci il linguaggio:", "[code={0}][/code]", 7);
-           
             // Hidden buttons -JustHvost
             createButton (">>>", function() {
                 // sorry for the bad code
-                var buttons = document.getElementsByClassName ("ntiBtn");
-                var hButtons= document.getElementsByClassName ("ntiHidden");
-                var isRight = this.value === ">>>";
+                var buttons = document.getElementsByClassName ("ntiBtn"),
+                    hButtons= document.getElementsByClassName ("ntiHidden"),
+                    isRight = this.value === ">>>";
                 for (var i = 0; i < buttons.length; i++)
                 {
                     buttons[i].style.display = ( isRight ? "none" : "block" );
@@ -238,6 +312,19 @@
                     this.style.marginLeft = ( IS_WEBKIT ? "2px" : "0px" );
                 this.value = ( isRight ? "<<<" : ">>>" );
             }, null, false, true);
+            createAppendButton ("B", "[b][/b]", 4, function() {
+                this.style.fontWeight = "bold";
+            }, true);
+            createAppendButton ("I", "[cur][/cur]", 6, function() {
+                this.style.fontStyle = "italic";
+            }, true);
+            createAppendButton ("U", "[u][/u]", 4, function() {
+                this.style.textDecoration = "underline";
+            }, true);
+            createAppendButton ("Del", "[del][/del]", 6, function() {
+                this.style.textDecoration = "line-through";
+            }, true);
+            createAppendButton ("Small", "[small][/small]", 8, null, true);
             createButton ("Gist", function() {
                 var link = prompt ("Inserisci il link o l'id del tuo Gist:", "");
                 if (link != null && link != "")
@@ -253,78 +340,64 @@
                     addToTextBox ("[gist]{0}[/gist]".format (link));
                 }
             }, null, true);
-            createAppendButton ("Wiki",    "[wiki=it][/wiki]", 7, null, true);
-            createAppendButton ("Quote", "[quote][/quote]", 8, null, true);
+            createAppendButton ("Wiki", "[wiki=it][/wiki]", 7, null, true);
+
+            createButton ("Quote", function() {
+                var nome = prompt ("Citazione di:", "");
+                if (nome != null && nome != "")
+                    addToTextBoxWithPos ("[quote={0}][/quote]".format (nome), 8);
+                else
+                    addToTextBoxWithPos ("[quote][/quote]", 8);
+            }, null, true);
             createAppendButton ("Hr", "[hr]", -1, null, true);
             createAppendButton ("Math", "[math][/math]", 7, null, true);
-            
-            //HOTKEYS ARE HERE, BITCHES!!! -JustHvost 
+            createButton ("?", function() {
+                alert ("Hotkey:\nAlt+1 => [b]\nAlt+2 => [cur]\nAlt+3 => [u]\nAlt+4 => [del]\nAlt+5 => [small]");
+            }, null, true);
+            //HOTKEYS ARE HERE, BITCHES!!! -JustHvost
+            //AND U INDENTED LIKE A BITCH!!! -Robertof
             var isAlt = false;
-		document.onkeyup=function(e) {
-    			if(e.which == 18) isAlt=false;
-		}
-		document.onkeydown=function(e){
-   			if(e.which == 18) isAlt=true;
-			if(e.which == 49 && isAlt == true) {
-         		addToTextBoxWithPos("[b][/b]", 4);
-         		return false;
-    			}
-    			if(e.which == 50 && isAlt == true) {
-         		addToTextBoxWithPos("[i][/i]", 4);
-         		return false;
-    			}
-    			if(e.which == 51 && isAlt == true) {
-         		addToTextBoxWithPos("[u][/u]", 4);
-         		return false;
-    			}
-    			if(e.which == 52 && isAlt == true) {
-         		addToTextBoxWithPos("[del][/del]", 6);
-         		return false;
-    			}
-    			if(e.which == 53 && isAlt == true) {
-         		addToTextBoxWithPos("[small][/small]", 8);
-         		return false;
-    			}
-		}
-		
-		
-            // add support for NERDZ protheme
-            // autodetect isn't supported since stylish css replacing is done in a weird
-            // way. let the user select if he wants protheme adjustments or not
-            // hook in the preferences page
-            if (/preferences\.php$/.test (document.location.href))
+            document.onkeyup = function (e) {
+                if (e.which == 18) isAlt = false;
+            };
+            document.onkeydown = function (e) {
+                if (e.which == 18) isAlt = true;
+                if (e.which == 49 && isAlt) { // 1
+                    addToTextBoxWithPos ("[b][/b]", 4);
+                    return false;
+                }
+                if (e.which == 50 && isAlt) { // 2
+                    addToTextBoxWithPos ("[cur][/cur]", 6); 
+                    return false;
+                }
+                if (e.which == 51 && isAlt) { // 3
+                    addToTextBoxWithPos ("[u][/u]", 4);
+                    return false;
+                }
+                if (e.which == 52 && isAlt) { // 4
+                    addToTextBoxWithPos ("[del][/del]", 6);
+                    return false;
+                }
+                if (e.which == 53 && isAlt) { // 5
+                    addToTextBoxWithPos ("[small][/small]", 8);
+                    return false;
+                }
+            };
+            // temporary fix for dev build users to show buttons
+            for (i in _AVAILABLE_BTNS)
             {
-                var nastyInterval = setInterval (function() {
-                    if (!document.getElementById ("edprofrm")) return;
-                    clearInterval (nastyInterval);
-                    var mainTr = document.createElement ("tr");
-                    var subTd1 = document.createElement ("td");
-                    subTd1.innerHTML = "<br>Enable NTI ProTheme support";
-                    var subTd2 = document.createElement ("td");
-                    var sChkbx = document.createElement ("input");
-                    sChkbx.setAttribute ("type", "checkbox");
-                    if (localStorage.NTIPThemeSupport === "true")
-                        sChkbx.checked = true;
-                    else
-                        sChkbx.checked = false;
-                    sChkbx.setAttribute ("id", "nti-protheme");
-                    var sApply = document.createElement ("input");
-                    sApply.setAttribute ("type", "button");
-                    sApply.setAttribute ("value", "Apply");
-                    sApply.addEventListener ("click", function() {
-                        // avoid using booleans in localStorage
-                        localStorage.NTIPThemeSupport = ( sChkbx.checked ? "true" : "false" );
-                    }, false);
-                    subTd2.innerHTML = "<br>";
-                    subTd2.appendChild (sChkbx);
-                    subTd2.appendChild (sApply);
-                    mainTr.appendChild (subTd1);
-                    mainTr.appendChild (subTd2);
-                    var trList = document.getElementsByTagName ("tr");
-                    // insert before the tr with the apply button
-                    document.getElementsByTagName ("tbody")[1].insertBefore (mainTr, trList[trList.length - 1]);
-                }, 1000);
+                if (_AVAILABLE_BTNS.hasOwnProperty (i))
+                    NERDZ_FORM.insertBefore (_AVAILABLE_BTNS[i][0], PMESSAGE);
             }
+            // Options API stuff begins now
+            OptionsAPI.registerCheckboxOption ("prothemesupport", "NTI: Robertof's Pro Theme Support", "Automatically saved when you check.");
+            OptionsAPI.registerCheckboxOption ("enablehotkeys",   "NTI: Enable Hotkeys", "Automatically saved when you check.");
+            OptionsAPI.registerOption         ("enabledbuttons",  "NTI: Enabled buttons", "Automatically saved when you make changes.", function() {
+                // this is gonna be quite interesting to code (LOL WTF!?)
+                // justhvost is faggot
+                
+            });
+            OptionsAPI.hookIfNecessary();
         }
     };
     // developer note: unsafeWindow is exported also on chrome, GM_info is not.
